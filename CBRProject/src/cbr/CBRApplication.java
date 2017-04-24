68fceb597f2eebc3e18a0050c734e685b303e75a
Luis Henrique Medeiros <lhmedeiros0@gmail.com>
2017-04-24 13:18:44 -0300

    Projeto alterado para contemplar mais alguns aspectos do jogo de truco

---
+++ CBRProject/src/cbr/CBRApplication.java
@@ -0,0 +1,336 @@
+package cbr;
+
+import jcolibri.cbraplications.StandardCBRApplication;
+import jcolibri.cbrcore.CBRCase;
+import jcolibri.cbrcore.CBRCaseBase;
+import jcolibri.cbrcore.CBRQuery;
+import jcolibri.cbrcore.Connector;
+import jcolibri.exception.ExecutionException;
+import jcolibri.exception.InitializingException;
+import jcolibri.method.retrieve.NNretrieval.NNConfig;
+
+import javax.annotation.Generated;
+import representation.DescCaso;
+import jcolibri.method.retrieve.NNretrieval.NNConfig;
+import jcolibri.cbrcore.Attribute;
+import jcolibri.method.retrieve.RetrievalResult;
+import jcolibri.method.retrieve.NNretrieval.NNScoringMethod;
+import jcolibri.method.retrieve.selection.SelectCases;
+import java.util.Collection;
+
+public class CBRApplication implements StandardCBRApplication {
+
+	@Generated(value = { "ColibriStudio" })
+	Connector connector;
+	
+	@Generated(value = { "ColibriStudio" })
+	CBRCaseBase casebase;
+
+
+
+	//******************************************************************/
+	// Configuration
+	//******************************************************************/
+
+	@Override
+	public void configure() throws ExecutionException {
+		try{
+			configureConnector();
+			configureCaseBase();
+		} catch (Exception e){
+			throw new ExecutionException(e);
+		}
+	}
+
+	/** Configures the connector */
+	@Generated(value = { "CS-PTConector" })	
+	private void configureConnector() throws InitializingException{
+		connector = new jcolibri.connector.PlainTextConnector();
+		connector.initFromXMLfile(jcolibri.util.FileIO
+				.findFile("config/plainTextConnectorConfig.xml"));
+	}
+
+	/** Configures the case base */
+	@Generated(value = { "CS-CaseManager" })	
+	private void configureCaseBase() throws InitializingException{
+		casebase = new jcolibri.casebase.LinearCaseBase();
+	}
+
+	//******************************************************************/
+	// Similarity
+	//******************************************************************/
+	
+	/** Configures the similarity */
+	@Generated(value = { "CS-Similarity" })	
+	private NNConfig getSimilarityConfig() {
+		NNConfig simConfig = new NNConfig();
+		simConfig
+				.setDescriptionSimFunction(new jcolibri.method.retrieve.NNretrieval.similarity.global.Average());
+		Attribute attribute0 = new Attribute("Carta3", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute0,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Threshold());
+		simConfig.setWeight(attribute0, 1.00);
+		Attribute attribute1 = new Attribute("Carta2", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute1,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Threshold());
+		simConfig.setWeight(attribute1, 1.00);
+		Attribute attribute2 = new Attribute("CasoId", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute2,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Threshold());
+		simConfig.setWeight(attribute2, 0.00);
+		Attribute attribute3 = new Attribute("Carta1", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute3,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Threshold());
+		simConfig.setWeight(attribute3, 1.00);
+		Attribute attribute4 = new Attribute("R1CartaMinha", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute4,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Threshold());
+		simConfig.setWeight(attribute4, 1.00);
+		Attribute attribute5 = new Attribute("R1CartaAdv", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute5,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Threshold());
+		simConfig.setWeight(attribute5, 1.00);
+		Attribute attribute6 = new Attribute("R1PediuEnvido", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute6,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute6, 1.00);
+		Attribute attribute7 = new Attribute("R1AceitouEnvido", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute7,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute7, 1.00);
+		Attribute attribute8 = new Attribute("R1PediuTruco", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute8,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute8, 1.00);
+		Attribute attribute9 = new Attribute("R1AceitouTruco", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute9,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute9, 1.00);
+		Attribute attribute10 = new Attribute("R1PediuRetruco", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute10,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute10, 1.00);
+		Attribute attribute11 = new Attribute("R1AceitouRetruco",
+				DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute11,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute11, 1.00);
+		Attribute attribute12 = new Attribute("R1PediuValeQuatro",
+				DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute12,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute12, 1.00);
+		Attribute attribute13 = new Attribute("R1AceitouValeQuatro",
+				DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute13,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute13, 1.00);
+		Attribute attribute14 = new Attribute("R2CartaMinha", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute14,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Threshold());
+		simConfig.setWeight(attribute14, 1.00);
+		Attribute attribute15 = new Attribute("R2CartaAdv", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute15,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Threshold());
+		simConfig.setWeight(attribute15, 1.00);
+		Attribute attribute16 = new Attribute("R2PediuEnvido", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute16,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute16, 1.00);
+		Attribute attribute17 = new Attribute("R2AceitouEnvido", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute17,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute17, 1.00);
+		Attribute attribute18 = new Attribute("R2PediuTruco", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute18,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute18, 1.00);
+		Attribute attribute19 = new Attribute("R2AceitouTruco", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute19,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute19, 1.00);
+		Attribute attribute20 = new Attribute("R2PediuRetruco", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute20,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute20, 1.00);
+		Attribute attribute21 = new Attribute("R2AceitouRetruco",
+				DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute21,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute21, 1.00);
+		Attribute attribute22 = new Attribute("R2PediuValeQuatro",
+				DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute22,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute22, 1.00);
+		Attribute attribute23 = new Attribute("R2AceitouValeQuatro",
+				DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute23,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute23, 1.00);
+		Attribute attribute24 = new Attribute("R3CartaMinha", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute24,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Threshold());
+		simConfig.setWeight(attribute24, 1.00);
+		Attribute attribute25 = new Attribute("R3CartaAdv", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute25,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Threshold());
+		simConfig.setWeight(attribute25, 1.00);
+		Attribute attribute26 = new Attribute("R3PediuEnvido", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute26,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute26, 1.00);
+		Attribute attribute27 = new Attribute("R3AceitouEnvido", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute27,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute27, 1.00);
+		Attribute attribute28 = new Attribute("R3PediuTruco", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute28,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute28, 1.00);
+		Attribute attribute29 = new Attribute("R3AceitouTruco", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute29,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute29, 1.00);
+		Attribute attribute30 = new Attribute("R3PediuRetruco", DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute30,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute30, 1.00);
+		Attribute attribute31 = new Attribute("R3AceitouRetruco",
+				DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute31,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute31, 1.00);
+		Attribute attribute32 = new Attribute("R3PediuValeQuatro",
+				DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute32,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute32, 1.00);
+		Attribute attribute33 = new Attribute("R3AceitouValeQuatro",
+				DescCaso.class);
+		simConfig
+				.addMapping(
+						attribute33,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Equal());
+		simConfig.setWeight(attribute33, 1.00);
+		return simConfig;
+	}
+
+	//******************************************************************/
+	// Methods
+	//******************************************************************/
+	
+
+	@Generated(value = { "ColibriStudio" })
+	@Override
+	public CBRCaseBase preCycle() throws ExecutionException {
+		casebase.init(connector);
+		return casebase;
+	}
+		
+	@Generated(value = { "ColibriStudio" })	
+	@Override
+	public void cycle(CBRQuery query) throws ExecutionException {
+		NNConfig simConfig = getSimilarityConfig();
+		Collection<RetrievalResult> eval = NNScoringMethod.evaluateSimilarity(
+				casebase.getCases(), query, simConfig);
+		eval = SelectCases.selectTopKRR(eval, 5);
+		System.out.println("Retrieved cases:");
+		for (RetrievalResult nse : eval)
+			System.out.println(nse);
+	}
+
+	@Generated(value = { "ColibriStudio" })
+	@Override
+	public void postCycle() throws ExecutionException {
+		connector.close();
+	}
+
+	/**
+	 * Main method
+	 */
+	@Generated(value = { "ColibriStudio" })
+	public static void main(String[] args) {
+		CBRApplication cbrApp = new CBRApplication();
+		try {
+			cbrApp.configure();
+			cbrApp.preCycle();
+
+			CBRQuery query = new CBRQuery();
+			query.setDescription(new DescCaso());
+			jcolibri.method.gui.formFilling.ObtainQueryWithFormMethod.obtainQueryWithoutInitialValues(query, null, null);
+			cbrApp.cycle(query);
+			cbrApp.postCycle();
+			
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
---
+++ CBRProject/src/representation/DescCaso.java
@@ -0,0 +1,433 @@
+package representation;
+
+import jcolibri.cbrcore.Attribute;
+import jcolibri.cbrcore.CaseComponent;
+ import colibristudio.annotations.JCOLIBRIAttributeType;
+
+
+public class DescCaso implements CaseComponent { 
+
+
+	/* Generated Class. Please Do Not Modify... */ 
+
+
+	private java.lang.Integer Carta3;
+
+
+	public java.lang.Integer getCarta3()
+		{
+			return Carta3;
+		}
+	public void setCarta3(java.lang.Integer Carta30)
+		{
+			this.Carta3 = Carta30;
+		}
+
+	private java.lang.Integer Carta2;
+
+
+	public java.lang.Integer getCarta2()
+		{
+			return Carta2;
+		}
+	public void setCarta2(java.lang.Integer Carta21)
+		{
+			this.Carta2 = Carta21;
+		}
+
+	private java.lang.Integer CasoId;
+
+
+	public java.lang.Integer getCasoId()
+		{
+			return CasoId;
+		}
+	public void setCasoId(java.lang.Integer CasoId2)
+		{
+			this.CasoId = CasoId2;
+		}
+
+	private java.lang.Integer Carta1;
+
+
+	public java.lang.Integer getCarta1()
+		{
+			return Carta1;
+		}
+	public void setCarta1(java.lang.Integer Carta13)
+		{
+			this.Carta1 = Carta13;
+		}
+
+	private java.lang.Integer R1CartaMinha;
+
+
+	public java.lang.Integer getR1CartaMinha()
+		{
+			return R1CartaMinha;
+		}
+	public void setR1CartaMinha(java.lang.Integer R1CartaMinha4)
+		{
+			this.R1CartaMinha = R1CartaMinha4;
+		}
+
+	private java.lang.Integer R1CartaAdv;
+
+
+	public java.lang.Integer getR1CartaAdv()
+		{
+			return R1CartaAdv;
+		}
+	public void setR1CartaAdv(java.lang.Integer R1CartaAdv5)
+		{
+			this.R1CartaAdv = R1CartaAdv5;
+		}
+
+	private java.lang.Boolean R1PediuEnvido;
+
+
+	public java.lang.Boolean getR1PediuEnvido()
+		{
+			return R1PediuEnvido;
+		}
+	public void setR1PediuEnvido(java.lang.Boolean R1PediuEnvido6)
+		{
+			this.R1PediuEnvido = R1PediuEnvido6;
+		}
+
+	private java.lang.Boolean R1AceitouEnvido;
+
+
+	public java.lang.Boolean getR1AceitouEnvido()
+		{
+			return R1AceitouEnvido;
+		}
+	public void setR1AceitouEnvido(java.lang.Boolean R1AceitouEnvido7)
+		{
+			this.R1AceitouEnvido = R1AceitouEnvido7;
+		}
+
+	private java.lang.Boolean R1PediuTruco;
+
+
+	public java.lang.Boolean getR1PediuTruco()
+		{
+			return R1PediuTruco;
+		}
+	public void setR1PediuTruco(java.lang.Boolean R1PediuTruco8)
+		{
+			this.R1PediuTruco = R1PediuTruco8;
+		}
+
+	private java.lang.Boolean R1AceitouTruco;
+
+
+	public java.lang.Boolean getR1AceitouTruco()
+		{
+			return R1AceitouTruco;
+		}
+	public void setR1AceitouTruco(java.lang.Boolean R1AceitouTruco9)
+		{
+			this.R1AceitouTruco = R1AceitouTruco9;
+		}
+
+	private java.lang.Boolean R1PediuRetruco;
+
+
+	public java.lang.Boolean getR1PediuRetruco()
+		{
+			return R1PediuRetruco;
+		}
+	public void setR1PediuRetruco(java.lang.Boolean R1PediuRetruco10)
+		{
+			this.R1PediuRetruco = R1PediuRetruco10;
+		}
+
+	private java.lang.Boolean R1AceitouRetruco;
+
+
+	public java.lang.Boolean getR1AceitouRetruco()
+		{
+			return R1AceitouRetruco;
+		}
+	public void setR1AceitouRetruco(java.lang.Boolean R1AceitouRetruco11)
+		{
+			this.R1AceitouRetruco = R1AceitouRetruco11;
+		}
+
+	private java.lang.Boolean R1PediuValeQuatro;
+
+
+	public java.lang.Boolean getR1PediuValeQuatro()
+		{
+			return R1PediuValeQuatro;
+		}
+	public void setR1PediuValeQuatro(java.lang.Boolean R1PediuValeQuatro12)
+		{
+			this.R1PediuValeQuatro = R1PediuValeQuatro12;
+		}
+
+	private java.lang.Boolean R1AceitouValeQuatro;
+
+
+	public java.lang.Boolean getR1AceitouValeQuatro()
+		{
+			return R1AceitouValeQuatro;
+		}
+	public void setR1AceitouValeQuatro(java.lang.Boolean R1AceitouValeQuatro13)
+		{
+			this.R1AceitouValeQuatro = R1AceitouValeQuatro13;
+		}
+
+	private java.lang.Integer R2CartaMinha;
+
+
+	public java.lang.Integer getR2CartaMinha()
+		{
+			return R2CartaMinha;
+		}
+	public void setR2CartaMinha(java.lang.Integer R2CartaMinha14)
+		{
+			this.R2CartaMinha = R2CartaMinha14;
+		}
+
+	private java.lang.Integer R2CartaAdv;
+
+
+	public java.lang.Integer getR2CartaAdv()
+		{
+			return R2CartaAdv;
+		}
+	public void setR2CartaAdv(java.lang.Integer R2CartaAdv15)
+		{
+			this.R2CartaAdv = R2CartaAdv15;
+		}
+
+	private java.lang.Boolean R2PediuEnvido;
+
+
+	public java.lang.Boolean getR2PediuEnvido()
+		{
+			return R2PediuEnvido;
+		}
+	public void setR2PediuEnvido(java.lang.Boolean R2PediuEnvido16)
+		{
+			this.R2PediuEnvido = R2PediuEnvido16;
+		}
+
+	private java.lang.Boolean R2AceitouEnvido;
+
+
+	public java.lang.Boolean getR2AceitouEnvido()
+		{
+			return R2AceitouEnvido;
+		}
+	public void setR2AceitouEnvido(java.lang.Boolean R2AceitouEnvido17)
+		{
+			this.R2AceitouEnvido = R2AceitouEnvido17;
+		}
+
+	private java.lang.Boolean R2PediuTruco;
+
+
+	public java.lang.Boolean getR2PediuTruco()
+		{
+			return R2PediuTruco;
+		}
+	public void setR2PediuTruco(java.lang.Boolean R2PediuTruco18)
+		{
+			this.R2PediuTruco = R2PediuTruco18;
+		}
+
+	private java.lang.Boolean R2AceitouTruco;
+
+
+	public java.lang.Boolean getR2AceitouTruco()
+		{
+			return R2AceitouTruco;
+		}
+	public void setR2AceitouTruco(java.lang.Boolean R2AceitouTruco19)
+		{
+			this.R2AceitouTruco = R2AceitouTruco19;
+		}
+
+	private java.lang.Boolean R2PediuRetruco;
+
+
+	public java.lang.Boolean getR2PediuRetruco()
+		{
+			return R2PediuRetruco;
+		}
+	public void setR2PediuRetruco(java.lang.Boolean R2PediuRetruco20)
+		{
+			this.R2PediuRetruco = R2PediuRetruco20;
+		}
+
+	private java.lang.Boolean R2AceitouRetruco;
+
+
+	public java.lang.Boolean getR2AceitouRetruco()
+		{
+			return R2AceitouRetruco;
+		}
+	public void setR2AceitouRetruco(java.lang.Boolean R2AceitouRetruco21)
+		{
+			this.R2AceitouRetruco = R2AceitouRetruco21;
+		}
+
+	private java.lang.Boolean R2PediuValeQuatro;
+
+
+	public java.lang.Boolean getR2PediuValeQuatro()
+		{
+			return R2PediuValeQuatro;
+		}
+	public void setR2PediuValeQuatro(java.lang.Boolean R2PediuValeQuatro22)
+		{
+			this.R2PediuValeQuatro = R2PediuValeQuatro22;
+		}
+
+	private java.lang.Boolean R2AceitouValeQuatro;
+
+
+	public java.lang.Boolean getR2AceitouValeQuatro()
+		{
+			return R2AceitouValeQuatro;
+		}
+	public void setR2AceitouValeQuatro(java.lang.Boolean R2AceitouValeQuatro23)
+		{
+			this.R2AceitouValeQuatro = R2AceitouValeQuatro23;
+		}
+
+	private java.lang.Integer R3CartaMinha;
+
+
+	public java.lang.Integer getR3CartaMinha()
+		{
+			return R3CartaMinha;
+		}
+	public void setR3CartaMinha(java.lang.Integer R3CartaMinha24)
+		{
+			this.R3CartaMinha = R3CartaMinha24;
+		}
+
+	private java.lang.Integer R3CartaAdv;
+
+
+	public java.lang.Integer getR3CartaAdv()
+		{
+			return R3CartaAdv;
+		}
+	public void setR3CartaAdv(java.lang.Integer R3CartaAdv25)
+		{
+			this.R3CartaAdv = R3CartaAdv25;
+		}
+
+	private java.lang.Boolean R3PediuEnvido;
+
+
+	public java.lang.Boolean getR3PediuEnvido()
+		{
+			return R3PediuEnvido;
+		}
+	public void setR3PediuEnvido(java.lang.Boolean R3PediuEnvido26)
+		{
+			this.R3PediuEnvido = R3PediuEnvido26;
+		}
+
+	private java.lang.Boolean R3AceitouEnvido;
+
+
+	public java.lang.Boolean getR3AceitouEnvido()
+		{
+			return R3AceitouEnvido;
+		}
+	public void setR3AceitouEnvido(java.lang.Boolean R3AceitouEnvido27)
+		{
+			this.R3AceitouEnvido = R3AceitouEnvido27;
+		}
+
+	private java.lang.Boolean R3PediuTruco;
+
+
+	public java.lang.Boolean getR3PediuTruco()
+		{
+			return R3PediuTruco;
+		}
+	public void setR3PediuTruco(java.lang.Boolean R3PediuTruco28)
+		{
+			this.R3PediuTruco = R3PediuTruco28;
+		}
+
+	private java.lang.Boolean R3AceitouTruco;
+
+
+	public java.lang.Boolean getR3AceitouTruco()
+		{
+			return R3AceitouTruco;
+		}
+	public void setR3AceitouTruco(java.lang.Boolean R3AceitouTruco29)
+		{
+			this.R3AceitouTruco = R3AceitouTruco29;
+		}
+
+	private java.lang.Boolean R3PediuRetruco;
+
+
+	public java.lang.Boolean getR3PediuRetruco()
+		{
+			return R3PediuRetruco;
+		}
+	public void setR3PediuRetruco(java.lang.Boolean R3PediuRetruco30)
+		{
+			this.R3PediuRetruco = R3PediuRetruco30;
+		}
+
+	private java.lang.Boolean R3AceitouRetruco;
+
+
+	public java.lang.Boolean getR3AceitouRetruco()
+		{
+			return R3AceitouRetruco;
+		}
+	public void setR3AceitouRetruco(java.lang.Boolean R3AceitouRetruco31)
+		{
+			this.R3AceitouRetruco = R3AceitouRetruco31;
+		}
+
+	private java.lang.Boolean R3PediuValeQuatro;
+
+
+	public java.lang.Boolean getR3PediuValeQuatro()
+		{
+			return R3PediuValeQuatro;
+		}
+	public void setR3PediuValeQuatro(java.lang.Boolean R3PediuValeQuatro32)
+		{
+			this.R3PediuValeQuatro = R3PediuValeQuatro32;
+		}
+
+	private java.lang.Boolean R3AceitouValeQuatro;
+
+
+	public java.lang.Boolean getR3AceitouValeQuatro()
+		{
+			return R3AceitouValeQuatro;
+		}
+	public void setR3AceitouValeQuatro(java.lang.Boolean R3AceitouValeQuatro33)
+		{
+			this.R3AceitouValeQuatro = R3AceitouValeQuatro33;
+		}
+
+
+	@Override
+	public Attribute getIdAttribute()
+		{
+			return new Attribute("CasoId",this.getClass());
+		} 
+
+	public String toString()		{
+			return "["+ R3PediuEnvido + " , " + R3AceitouRetruco + " , " + R2PediuTruco + " , " + R3AceitouValeQuatro + " , " + R2CartaMinha + " , " + Carta2 + " , " + R3AceitouEnvido + " , " + Carta1 + " , " + R1AceitouEnvido + " , " + Carta3 + " , " + R1CartaAdv + " , " + R2CartaAdv + " , " + R2AceitouEnvido + " , " + R3CartaAdv + " , " + R2AceitouValeQuatro + " , " + R2AceitouRetruco + " , " + R1PediuEnvido + " , " + R1AceitouTruco + " , " + R3PediuTruco + " , " + R3CartaMinha + " , " + R3AceitouTruco + " , " + R1PediuTruco + " , " + R1CartaMinha + " , " + R1PediuValeQuatro + " , " + R3PediuRetruco + " , " + R1PediuRetruco + " , " + R2PediuEnvido + " , " + R1AceitouValeQuatro + " , " + R2PediuRetruco + " , " + R3PediuValeQuatro + " , " + CasoId + " , " + R2PediuValeQuatro + " , " + R1AceitouRetruco + " , " + R2AceitouTruco +"]";
+		}
+
+}
